69db767dc22a6a26f94daf20b17f777d8b2da26a
ophir <devteam@wizer.me>
2019-01-12 08:09:24 +0200

    add redux to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
+import { simpleAction } from './actions/simpleAction'
+
+
+const mapStateToProps = state => ({
+  ...state
+ })
+
+ const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        </div>
-    );
-  }
-}
 
-export default App;
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
+
+  
+ render() {
+  return (
+   <div className="App">
+    <header className="App-header">
+     <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+    <button onClick={this.simpleAction}>Test redux action</button>
+    </p>
+    <hr/>
+    <pre>
+ {
+  JSON.stringify(this.props)
+ }
+</pre>
+   </div>
+   
+  );
+ }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
