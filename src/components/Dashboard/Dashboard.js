8534c7daeb77307245d7a2aad6c154df8951144b
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-03 22:26:17 +0200

    show all 6 charts

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -19,8 +19,9 @@ class Dashboard extends Component{
     render(){
         return (<Container>
             {/* <Row><Col><ControlConsole/></Col><Col>{<RaceMap lat={this.props.data.gps_lat} long={this.props.data.gps_long}/>}</Col></Row>*/}
-            <Row><Col><ChartMonitor label="Water Temprature" dataField = 'eng_wat_temp'/></Col><Col><ChartMonitor label="Oil Temprature" dataField = 'eng_oil_temp'/></Col><Col><ChartMonitor label="Temprature (C)" dataField = 'eng_wat_temp'/></Col></Row>
-            <Row><Col><ChartMonitor label="Hydraulic Pressure" dataField = "hydraulic_pres"/></Col><Col><ChartMonitor label="Oil Pressure" dataField = "eng_oil_pres"/></Col><Col><ChartMonitor label="Temprature (C)" dataField = 'eng_wat_temp'/></Col></Row>
+            <Row><Col><ChartMonitor label="Eng. Oil Temprature" dataField = 'eng_oil_temp'/></Col><Col><ChartMonitor label="Eng. Water Temprature" dataField = 'eng_wat_temp'/></Col></Row>
+            <Row><Col><ChartMonitor label="Eng. Oil Pressure" dataField = "eng_oil_pres"/></Col><Col><ChartMonitor label="Eng. Hydraulic Pressure" dataField = "hydraulic_pres"/></Col></Row>
+            <Row><Col><ChartMonitor label="Differential Temprature" dataField =  "diff_oiltemp"/></Col><Col><ChartMonitor label="Eng. Fuel Pressure" dataField = "fuel_pres"/></Col></Row>
 
             </Container>)
     }
