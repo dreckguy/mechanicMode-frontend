98fcbf4380458e7adb8ef2d68597a1605ea59b54
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-04 16:36:31 +0200

    turned data into arrays of values

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,7 +1,20 @@
+import _ from 'lodash'
+
 export default (state = {}, action) => {
   switch(action.type){
     case 'FETCH_DATA':
-      return Object.assign({}, {...state, data:action.data});
+    const newData = action.data;
+    const values = _.cloneDeep(state.data);
+    for (let valueType in newData) {
+      if (newData.hasOwnProperty(valueType)) {
+        if(!Array.isArray(values[valueType])){
+          values[valueType] = []; 
+        }
+
+        values[valueType].push(newData[valueType])
+      }
+  }
+      return Object.assign({}, {...state, data:values});
     default:
       return state;
   }
