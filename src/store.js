d513dd171eb93dc2c3e7ebd30e1dfbe0aa2a425d
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-06 18:43:17 +0200

    start from yarin lap static data

--- src/store.js
+++ src/store.js
@@ -3,14 +3,18 @@ import { createStore, applyMiddleware } from 'redux';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 import mainReducer from './reducers/mainReducer'
+
+import yarinLap from './data/yarin_lap.json';
+const staticData = yarinLap.data;
+
 require('dotenv').config({ path: './.env' })
- 
+
 
 const BACKEND_ADDRESS = process.env.REACT_APP_BACKEND_ADDRESS
 const SOCKET_PORT = process.env.REACT_APP_SOCKET_PORT
 const SERVER_PREFACE = process.env.REACT_APP_SERVER_PREFACE
 
-const preloadedState ={data:{}}
+const preloadedState = {data:staticData};
 
 
 
