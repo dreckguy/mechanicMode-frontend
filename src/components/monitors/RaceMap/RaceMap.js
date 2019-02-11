7b19eb713b160386414be83db13b7194c6e4f901
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-11 11:18:14 +0200

    connect map

--- src/components/monitors/RaceMap/RaceMap.js
+++ src/components/monitors/RaceMap/RaceMap.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './RaceMap.css'
 
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 const mapStyles = {
     width: '100vh',
     height: '100%'
   };
 
+  const GRIIIP_HOME={lat: 32.105208,long:34.898308}
+
+  const mapStateToProps = (state, ownProps) => {
+
+
+    const last = state.last;
+
+    if(!last){
+        return {lat:GRIIIP_HOME.lat, long:GRIIIP_HOME.long};
+    }
+
+      return {
+        lat: parseFloat(last["gps_lat"]),
+        long: parseFloat(last["gps_long"])
+      }
+    }
+
+
 class MapContainer extends Component {
     render() {
+
+      console.log(this.props)
 
       return (
         <Map
@@ -17,8 +39,8 @@ class MapContainer extends Component {
           zoom={14}
           style={mapStyles}
           initialCenter={{
-           lat: -1.2884,
-           lng: 36.8233
+           lat: GRIIIP_HOME.lat,
+           lng: GRIIIP_HOME.long
           }}
         />
       );
@@ -27,4 +49,4 @@ class MapContainer extends Component {
 
   export default GoogleApiWrapper({
     apiKey: GOOGLE_API_KEY
-  })(MapContainer);
+  })(connect(mapStateToProps)(MapContainer));
