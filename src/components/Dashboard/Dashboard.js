71c6f4b1642513f471846e8fc4da7cfb1531954a
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-25 14:56:55 +0200

    put everything in rows inside each container

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -11,7 +11,7 @@ class Dashboard extends Component{
     render(){
         return (<Container>
              <Row noGutters={true}><Col><ControlConsole/></Col><Col><RaceMap/></Col></Row>
-             <ChartsPanel/>
+             <Row><ChartsPanel/></Row>
             </Container>)
     }
 
