6bfab1bcb97d38aa581742f950a009222f733ce9
Ophirus Magnivus <dreckguy@gmail.com>
2019-02-27 11:54:24 +0200

    fix relativr size of charts

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,8 @@
 Set-Variable -Name "IMAGE" -Value "dreckguy/mechanicmode-frontend"
+Set-Variable -Name "KEY_PATH" -Value "~/keys/dashboard.pem"
+Set-Variable -Name "SERVER" -Value "ec2-user@dev.dashboard.griiip.com"
+
+
 docker build -t $(Get-Variable -Name "IMAGE" -ValueOnly) .
 docker push $(Get-Variable -Name "IMAGE" -ValueOnly)
+ssh -i $(Get-Variable -Name "KEY_PATH" -ValueOnly) $(Get-Variable -Name "SERVER" -ValueOnly) sh frontend.sh
--- src/components/monitors/ChartMonitor/ChartMonitor.js
+++ src/components/monitors/ChartMonitor/ChartMonitor.js
@@ -1,6 +1,8 @@
 import {connect} from 'react-redux'
 import React, { Component} from 'react';
 import Dygraph from 'dygraphs';
+import './ChartMonitor.css';
+
 
 const mapStateToProps = (state, ownProps) => {
 
