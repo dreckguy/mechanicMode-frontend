0f3b989e001bceec9c8e51a0c69c3d7794793c2a
Ophirus Magnivus <dreckguy@gmail.com>
2019-03-21 09:32:03 +0200

    add registerer to dashboard

--- src/components/ControlConsole/ControlConsole.js
+++ src/components/ControlConsole/ControlConsole.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './ControlConsole.css'
 import {Container,Col,Row} from 'react-bootstrap';
 import Gauge from '../monitors/GaugeMonitor/GaugeMonitor';
+import Registerer from '../Registerer';
 
 export default class ControlConsole extends Component{
     render(){
         return (<Container>
+            <Row><Registerer/></Row>
 <Row><Col><Gauge label="Speed" max={250} dataField = "gps_speed"/></Col><Col><Gauge label="RPM" max={15000} dataField={'rpm'}/></Col></Row>           
 </Container>)
     }
