ff045a80c2d3da1cb0cbcba8b91d0b7bf6f59e43
Ophirus Magnivus <dreckguy@gmail.com>
2019-04-22 10:44:17 +0300

    get back race map trail

--- src/components/monitors/RaceMap/RaceMap.js
+++ src/components/monitors/RaceMap/RaceMap.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-import { Map,Marker,GoogleApiWrapper } from 'google-maps-react';
+import { Map,Marker,Polyline,GoogleApiWrapper } from 'google-maps-react';
 import './RaceMap.css'
 
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 const DEFAULT_ZOOM = parseInt(process.env.REACT_APP_DEFAULT_ZOOM);
+const TRAIL_MAX_POINTS = parseInt(process.env.REACT_APP_MAP_TRAIL_MAX_POINTS);
+
 
 const GRIIIP_HOME={lat: 32.105208,lng:34.898308}
 const DEFAULT_LOCATION = GRIIIP_HOME;
@@ -32,8 +34,14 @@ const mapStyles = {
       }else{
           newLocation = DEFAULT_LOCATION;
         }
+
+        const lastValues = state.data.slice(-TRAIL_MAX_POINTS).map((data)=>{
+          return {lat: parseFloat(data["gps_lat"]),lng: parseFloat(data["gps_long"])};
+
+        });
+
     
-        return {location: newLocation}
+        return {location: newLocation, trail:lastValues}
     }
 
 
@@ -67,6 +75,16 @@ class MapContainer extends Component {
       scale: 3
   }}
     />
+
+<Polyline
+  path={this.props.trail} 
+  options={{ 
+  strokeColor: '#201a2d',
+  strokeOpacity: 0.8,
+  strokeWeight: 4
+}}
+/>
+
         </Map>
       </div>);
     }
